dd12b964d35b04dd9177ee6d0b59c97c48a78af0
seanyinx <sean.yin@huawei.com>
2017-09-10 18:06:51 +0800

    JAV-201 demo with discovery using service center

--- saga-demo/flight-booking-service/src/main/java/io/servicecomb/saga/demo/flight/booking/FlightBookingApplication.java
+++ saga-demo/flight-booking-service/src/main/java/io/servicecomb/saga/demo/flight/booking/FlightBookingApplication.java
@@ -16,10 +16,12 @@
 
 package io.servicecomb.saga.demo.flight.booking;
 
+import io.servicecomb.springboot.starter.provider.EnableServiceComb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableServiceComb
 public class FlightBookingApplication {
 
   public static void main(String[] args) {
