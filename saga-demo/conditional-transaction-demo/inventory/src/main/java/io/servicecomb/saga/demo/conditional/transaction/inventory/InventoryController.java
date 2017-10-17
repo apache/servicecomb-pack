aaa55cec51c19bfe4e9c661896b2484da6216436
seanyinx <sean.yin@huawei.com>
2017-10-17 14:14:55 +0800

    JAV-431 added conditional inventory service
    Signed-off-by: seanyinx <sean.yin@huawei.com>

--- saga-demo/conditional-transaction-demo/inventory/src/main/java/io/servicecomb/saga/demo/conditional/transaction/inventory/InventoryController.java
+++ saga-demo/conditional-transaction-demo/inventory/src/main/java/io/servicecomb/saga/demo/conditional/transaction/inventory/InventoryController.java
@@ -14,17 +14,16 @@
  * limitations under the License.
  */
 
-package io.servicecomb.saga.demo.conditional.transaction.payment;
+package io.servicecomb.saga.demo.conditional.transaction.inventory;
 
-import static java.util.Collections.singletonMap;
+import static java.util.Collections.singleton;
 import static org.springframework.http.HttpStatus.FORBIDDEN;
 import static org.springframework.http.MediaType.APPLICATION_FORM_URLENCODED_VALUE;
 import static org.springframework.http.MediaType.TEXT_PLAIN_VALUE;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 import static org.springframework.web.bind.annotation.RequestMethod.PUT;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Set;
 import java.util.UUID;
 
 import org.springframework.http.ResponseEntity;
@@ -34,48 +33,47 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/")
-public class PaymentController {
-  private static final int PRICE = 400;
-  private static final int UPGRADE_THRESHOLD = 1000;
+public class InventoryController {
+  private static final int FETCH_THRESHOLD = 10;
 
-  private final Map<String, Integer> customerPurchases = new HashMap<>(singletonMap("mike", 0));
+  private final Set<String> customerPurchases = singleton("mike");
+  private int stock = 11;
 
-  @RequestMapping(value = "payment", method = POST, consumes = APPLICATION_FORM_URLENCODED_VALUE, produces = TEXT_PLAIN_VALUE)
-  public ResponseEntity<String> purchase(@RequestParam String customerId) {
-    if (!customerPurchases.containsKey(customerId)) {
+  @RequestMapping(value = "inventory", method = POST, consumes = APPLICATION_FORM_URLENCODED_VALUE, produces = TEXT_PLAIN_VALUE)
+  public ResponseEntity<String> dispatch(@RequestParam String customerId) {
+    if (!customerPurchases.contains(customerId)) {
       return new ResponseEntity<>("No such customer with id " + customerId, FORBIDDEN);
     }
 
-    customerPurchases.compute(customerId, (id, purchases) -> purchases + PRICE);
-
-    if (isUpgradable(customerId)) {
+    stock--;
+    if (isStockShort()) {
       return response(customerId, "");
     }
 
-    return response(customerId, ",  \"sagaChildren\": [\"inventory\"] \n");
+    return response(customerId,",  \"sagaChildren\": [\"none\"] \n");
   }
 
-  @RequestMapping(value = "payment", method = PUT, consumes = APPLICATION_FORM_URLENCODED_VALUE, produces = TEXT_PLAIN_VALUE)
-  public ResponseEntity<String> refund(@RequestParam String customerId) {
-    if (!customerPurchases.containsKey(customerId)) {
+  @RequestMapping(value = "inventory", method = PUT, consumes = APPLICATION_FORM_URLENCODED_VALUE, produces = TEXT_PLAIN_VALUE)
+  public ResponseEntity<String> recall(@RequestParam String customerId) {
+    if (!customerPurchases.contains(customerId)) {
       return new ResponseEntity<>("No such customer with id " + customerId, FORBIDDEN);
     }
 
-    return ResponseEntity.ok(String.format("Payment refunded with id %s for customer %s",
+    return ResponseEntity.ok(String.format("Dispatch recalled with id %s for customer %s",
         UUID.randomUUID().toString(),
         customerId));
   }
 
   private ResponseEntity<String> response(String customerId, String extra) {
     return ResponseEntity.ok(String.format("{\n"
-            + "  \"body\": \"Payment made with id %s for customer %s\"\n"
+            + "  \"body\": \"Goods dispatched with id %s for customer %s\"\n"
             + extra
             + "}",
         UUID.randomUUID().toString(),
         customerId));
   }
 
-  private boolean isUpgradable(String customerId) {
-    return customerPurchases.get(customerId) >= UPGRADE_THRESHOLD;
+  private boolean isStockShort() {
+    return stock <= FETCH_THRESHOLD;
   }
 }
