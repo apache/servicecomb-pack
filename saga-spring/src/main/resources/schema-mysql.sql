a6742975973048806c9f94cdc73a486a22adc52e
saga <spring_holy@163.com>
2017-11-14 16:48:16 +0800

    Modify the DDL statement
    Original content：
    `creation_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
    Modified content：
    `creation_time` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6)
    
    This operation is to solve this problem https://github.com/ServiceComb/ServiceComb-Saga/issues/68

--- saga-spring/src/main/resources/schema-mysql.sql
+++ saga-spring/src/main/resources/schema-mysql.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS `saga_event_entity` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `saga_id` varchar(36) NOT NULL,
-  `creation_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `creation_time` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
   `type` varchar(50) NOT NULL,
   `content_json` JSON NOT NULL,
   PRIMARY KEY (`id`),
