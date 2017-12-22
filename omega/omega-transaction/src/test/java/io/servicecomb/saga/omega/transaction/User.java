2e0945f5deaa53ef576b0f77cca169f7ea549775
seanyinx <sean.yin@huawei.com>
2017-12-22 16:13:56 +0800

    SCB-96 sent serialized message pre transactional
    Signed-off-by: seanyinx <sean.yin@huawei.com>

---
+++ omega/omega-transaction/src/test/java/io/servicecomb/saga/omega/transaction/User.java
@@ -0,0 +1,49 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.servicecomb.saga.omega.transaction;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+  @Id
+  @GeneratedValue
+  private long id;
+
+  private String username;
+  private String email;
+
+  public User(String username, String email) {
+    this.username = username;
+    this.email = email;
+  }
+
+  public long id() {
+    return id;
+  }
+
+  public String username() {
+    return username;
+  }
+
+  public String email() {
+    return email;
+  }
+}
