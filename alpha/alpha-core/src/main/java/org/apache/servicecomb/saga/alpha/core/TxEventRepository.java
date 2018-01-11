50444100ddc4d906b010eefa42d974bb219592ff
seanyinx <sean.yin@huawei.com>
2018-01-11 10:07:47 +0800

    SCB-212 compensated on TxEndedEvent immediately if global TX already failed, in case of timeout
    Signed-off-by: seanyinx <sean.yin@huawei.com>

--- alpha/alpha-core/src/main/java/org/apache/servicecomb/saga/alpha/core/TxEventRepository.java
+++ alpha/alpha-core/src/main/java/org/apache/servicecomb/saga/alpha/core/TxEventRepository.java
@@ -22,5 +22,5 @@ import java.util.List;
 public interface TxEventRepository {
   void save(TxEvent event);
 
-  List<TxEvent> findStartedTransactions(String globalTxId, String type);
+  List<TxEvent> findTransactions(String globalTxId, String type);
 }
