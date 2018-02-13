da07faa00be97a6bd0793044f609e2890a36eaa5
Eric Lee <dagang.li@huawei.com>
2018-02-13 15:50:31 +0800

    SCB-243 able to intercept old java chassis requests (#136)
    * SCB-243 able to intercept old java chassis requests
    
    Signed-off-by: Eric Lee <dagang.li@huawei.com>
    
    * SCB-243 add license header
    
    Signed-off-by: Eric Lee <dagang.li@huawei.com>

--- omega/omega-context/src/test/java/org/apache/servicecomb/saga/omega/context/UniqueIdGeneratorTest.java
+++ omega/omega-context/src/test/java/org/apache/servicecomb/saga/omega/context/UniqueIdGeneratorTest.java
@@ -6,17 +6,16 @@
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
  */
 
-package org.apache.servicecomb.saga.omega.transport.resttemplate;
+package org.apache.servicecomb.saga.omega.context;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
@@ -32,7 +31,6 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
-import org.apache.servicecomb.saga.omega.context.UniqueIdGenerator;
 import org.junit.Test;
 
 public class UniqueIdGeneratorTest {
