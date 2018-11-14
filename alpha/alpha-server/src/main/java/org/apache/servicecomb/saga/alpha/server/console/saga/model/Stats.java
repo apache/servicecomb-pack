ec73b690d48dd5e7185eb2be48809bb748f261f9
asifdxtreme <mohammad.asif.siddiqui1@huawei.com>
2018-11-14 17:45:03 +0530

    Add Unit Test for Alpha Console API's

--- alpha/alpha-server/src/main/java/org/apache/servicecomb/saga/alpha/server/console/saga/model/Stats.java
+++ alpha/alpha-server/src/main/java/org/apache/servicecomb/saga/alpha/server/console/saga/model/Stats.java
@@ -20,6 +20,36 @@ import java.util.Date;
 
 public class Stats {
 
+  private int totalTransactions;
+
+  private int pendingTransactions;
+
+  private int committedTransactions;
+
+  private int compensatingTransactions;
+
+  private int rollbackTransactions;
+
+  private Date updatedAt;
+
+  private int failureRate;
+
+  public Stats(int totalTransactions, int pendingTransactions, int committedTransactions, int compensatingTransactions,
+      int rollbackTransactions) {
+    setTotalTransactions(totalTransactions);
+    setPendingTransactions(pendingTransactions);
+    setCommittedTransactions(committedTransactions);
+    setCompensatingTransactions(compensatingTransactions);
+    setRollbackTransactions(rollbackTransactions);
+    if (totalTransactions > 0 && rollbackTransactions + compensatingTransactions > 0) {
+      setFailureRate((rollbackTransactions + compensatingTransactions) * 100 / totalTransactions);
+    } else {
+      setFailureRate(0);
+    }
+
+    setUpdatedAt(new Date());
+  }
+
   public int getTotalTransactions() {
     return totalTransactions;
   }
@@ -27,8 +57,6 @@ public class Stats {
   public void setTotalTransactions(int totalTransactions) {
     this.totalTransactions = totalTransactions;
   }
-
-  private int totalTransactions;
 
   public int getPendingTransactions() {
     return pendingTransactions;
@@ -62,14 +90,6 @@ public class Stats {
     this.rollbackTransactions = rollbackTransactions;
   }
 
-  private int pendingTransactions;
-
-  private int committedTransactions;
-
-  private int compensatingTransactions;
-
-  private int rollbackTransactions;
-
   public Date getUpdatedAt() {
     return updatedAt;
   }
@@ -78,26 +98,11 @@ public class Stats {
     this.updatedAt = updatedAt;
   }
 
-  private Date updatedAt;
-
   public int getFailureRate() {
     return failureRate;
   }
 
   public void setFailureRate(int failureRate) {
     this.failureRate = failureRate;
-  }
-
-  private int failureRate;
-
-  public Stats(int totalTransactions, int pendingTransactions, int committedTransactions, int compensatingTransactions,
-      int rollbackTransactions) {
-    setTotalTransactions(totalTransactions);
-    setPendingTransactions(pendingTransactions);
-    setCommittedTransactions(committedTransactions);
-    setCompensatingTransactions(compensatingTransactions);
-    setRollbackTransactions(rollbackTransactions);
-    setFailureRate(rollbackTransactions + compensatingTransactions / totalTransactions);
-    setUpdatedAt(new Date());
   }
 }
