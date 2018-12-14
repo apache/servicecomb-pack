1496a8a33b87cffea5d7576786b4d7eb4da4f0f9
Willem Jiang <jiangning9@huawei.com>
2018-12-14 16:41:49 +0800

    SCB-976 Updated the package names of demo

---
+++ demo/saga-spring-demo/booking/src/main/java/org/apache/servicecomb/pack/demo/booking/BookingController.java
@@ -0,0 +1,67 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.servicecomb.pack.demo.booking;
+
+import org.apache.servicecomb.pack.omega.context.annotations.SagaStart;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class BookingController {
+
+  @Value("${car.service.address:http://car.servicecomb.io:8080}")
+  private String carServiceUrl;
+
+  @Value("${hotel.service.address:http://hotel.servicecomb.io:8080}")
+  private String hotelServiceUrl;
+
+  @Autowired
+  private RestTemplate template;
+
+  @SagaStart
+  @PostMapping("/booking/{name}/{rooms}/{cars}")
+  public String order(@PathVariable String name,  @PathVariable Integer rooms, @PathVariable Integer cars) {
+    template.postForEntity(
+        carServiceUrl + "/order/{name}/{cars}",
+        null, String.class, name, cars);
+
+    postCarBooking();
+
+    template.postForEntity(
+        hotelServiceUrl + "/order/{name}/{rooms}",
+        null, String.class, name, rooms);
+
+    postBooking();
+
+    return name + " booking " + rooms + " rooms and " + cars + " cars OK";
+  }
+
+  // This method is used by the byteman to inject exception here
+  private void postCarBooking() {
+
+  }
+
+  // This method is used by the byteman to inject the faults such as the timeout or the crash
+  private void postBooking() {
+
+  }
+}
