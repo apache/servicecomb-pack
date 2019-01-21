601a15e68287213bf6ef0b8ede2fc5a354499743
longchun <longchun@gmail.com>
2019-01-21 16:24:00 +0800

    SCB-1119 Add the order number on the demo

--- demo/saga-spring-demo/car/src/main/java/org/apache/servicecomb/pack/demo/car/CarBooking.java
+++ demo/saga-spring-demo/car/src/main/java/org/apache/servicecomb/pack/demo/car/CarBooking.java
@@ -23,7 +23,6 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @JsonAutoDetect(fieldVisibility = Visibility.ANY)
 class CarBooking {
-  @JsonIgnore
   private Integer id;
   private String name;
   private Integer amount;
