4cc58ef46283944f7e3e46b4b3f163427db55234
张磊 <zhang_lei@boco.com.cn>
2019-01-22 10:16:48 +0800

    Add omega instanceid to properties

--- omega/omega-spring-starter/src/main/java/org/apache/servicecomb/pack/omega/spring/OmegaSpringConfig.java
+++ omega/omega-spring-starter/src/main/java/org/apache/servicecomb/pack/omega/spring/OmegaSpringConfig.java
@@ -71,7 +71,6 @@ class OmegaSpringConfig {
   ServiceConfig serviceConfig(@Value("${spring.application.name}") String serviceName, @Value("${alpha.instance.instanceId:#{null}}") String instanceId) {
     return new ServiceConfig(serviceName,instanceId);
   }
-  
 
   @Bean
   ParametersContext parametersContext() {
