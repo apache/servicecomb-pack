c61b117b34363ddc21c82b977977033bfafec26f
张磊 <coolbeevip@gmail.com>
2019-01-24 21:53:50 +0800

    code format

--- omega/omega-spring-cloud-starter/src/main/java/org/apache/servicecomb/pack/omega/spring/cloud/OmegaSpringEurekaConfig.java
+++ omega/omega-spring-cloud-starter/src/main/java/org/apache/servicecomb/pack/omega/spring/cloud/OmegaSpringEurekaConfig.java
@@ -37,7 +37,6 @@ import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EurekaDiscoveryClientConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import java.util.ArrayList;
 import java.util.List;
 
