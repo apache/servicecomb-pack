450685bbe890aa640c1af1fb3cf9b9ae29a5f0a2
Willem Jiang <jiangning9@huawei.com>
2019-03-01 20:24:59 +0800

    Removed the gpg keys as we do the release by hand

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -17,29 +17,7 @@
 ## ---------------------------------------------------------------------------
 #bin/sh
 
-##Check if the commit is tagged commit or not
-TAGGEDCOMMIT=$(git tag -l --contains HEAD)
-if [ "$TAGGEDCOMMIT" == "" ]; then
-        TAGGEDCOMMIT=false
-else
-        TAGGEDCOMMIT=true
-fi
-echo $TAGGEDCOMMIT
-
-if [ "$TAGGEDCOMMIT" ==   "true" ]; then
-    echo "Decrypting the key"
-		openssl aes-256-cbc -K $encrypted_03bd1399b9b3_key -iv $encrypted_03bd1399b9b3_iv -in gpg-sec.tar.enc -out .github/gpg-sec.tar -d
-    tar xvf gpg-sec.tar
-		echo "Deploying Staging Release"
-		mvn deploy -DskipTests --settings .travis.settings.xml -Ppassphrase -Prelease 
-    if [ $? == 0 ]; then
-			echo "${green}Staging Deployment is Success, please log on to Nexus Repo to see the staging release..${reset}"
-		else
-			echo "${red}Staging Release deployment failed.${reset}"
-			exit 1
-		fi
-else
-		echo "Deploy a Non-Signed Staging Release"
+echo "Deploy a Non-Signed Staging Release"
 		mvn deploy -Ddocker.skip -DskipTests --settings .travis.settings.xml
     if [ $? == 0 ]; then
 			echo "${green}Snapshot Deployment is Success, please log on to Nexus Repo to see the snapshot release..${reset}"
@@ -47,5 +25,4 @@ else
 			echo "${red}Snapshot deployment failed.${reset}"
 			# No need to exit 1 here as the snapshot depoyment will fail for private builds as decryption of password is allowed for ServiceComb repo and not forked repo's.
 		fi
-fi
 echo "Deployment Completed"
