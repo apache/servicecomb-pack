3c6ec6355008fbac8b50927bbfb8f89bbe986e42
张磊 <coolbeevip@gmail.com>
2019-03-25 18:50:09 +0800

    SCB-1217 Consul support random gRPC port

--- alpha/alpha-spring-cloud-starter-consul/src/main/java/org/apache/servicecomb/pack/alpha/server/discovery/consul/AlphaConsulAutoConfiguration.java
+++ alpha/alpha-spring-cloud-starter-consul/src/main/java/org/apache/servicecomb/pack/alpha/server/discovery/consul/AlphaConsulAutoConfiguration.java
@@ -18,39 +18,61 @@
 package org.apache.servicecomb.pack.alpha.server.discovery.consul;
 
 import com.ecwid.consul.v1.ConsulClient;
+import com.ecwid.consul.v1.Response;
+import com.ecwid.consul.v1.agent.model.NewService;
+import com.ecwid.consul.v1.catalog.model.CatalogService;
+import com.google.common.eventbus.EventBus;
+import com.google.common.eventbus.Subscribe;
+import java.util.List;
+import org.apache.servicecomb.pack.alpha.core.event.GrpcStartableStartedEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.cloud.client.discovery.event.InstanceRegisteredEvent;
 import org.springframework.cloud.consul.ConsulAutoConfiguration;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.cloud.consul.discovery.ConsulDiscoveryProperties;
 
 import javax.annotation.PostConstruct;
 import java.lang.invoke.MethodHandles;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
 
-@Configuration
+@Component
 @ConditionalOnProperty(value = {"spring.cloud.consul.enabled"})
 @AutoConfigureAfter(ConsulAutoConfiguration.class)
 public class AlphaConsulAutoConfiguration {
 
   private static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
+  private String consuleInstanceId;
+
+  private int actualAlphaServerPort;
+
+  @Value("${alpha.server.port}")
+  private int alphaServerPort;
+
   @Autowired
   private ConsulClient consulClient;
 
-  @Value("${spring.cloud.consul.discovery.instanceId}")
-  private String consuleInstanceId;
+  @Autowired
+  @Qualifier("alphaEventBus")
+  EventBus eventBus;
+
+  @Autowired
+  ConsulDiscoveryProperties consulDiscoveryProperties;
 
   @PostConstruct
   public void init(){
+    eventBus.register(this);
     // Unregister from consul when shutdown
     Runtime.getRuntime().addShutdownHook(new Thread() {
       public void run() {
-        String remoteConsuleInstanceId = getRemoteConsulInstanceId();
-        LOG.info("Unregister Consul {}", remoteConsuleInstanceId);
-        consulClient.agentServiceDeregister(remoteConsuleInstanceId);
+        LOG.info("Unregister Consul {}", consuleInstanceId);
+        consulClient.agentServiceDeregister(consuleInstanceId);
       }
     });
   }
@@ -58,7 +80,45 @@ public class AlphaConsulAutoConfiguration {
   /**
    * Format local consul instanceId to instanceId of consul server
    * */
-  private String getRemoteConsulInstanceId() {
+  private String formatConsulInstanceId(String consuleInstanceId) {
     return consuleInstanceId.replace(".", "-").replace(":", "-");
+  }
+
+  /**
+   * Update actual grpc port to actualAlphaServerPort
+   * */
+  @Subscribe
+  public void listenGrpcStartableStartedEvent(GrpcStartableStartedEvent grpcStartableStartedEvent) {
+    if(alphaServerPort == 0){
+      actualAlphaServerPort = grpcStartableStartedEvent.getPort();
+    }
+  }
+
+  /**
+   * Update grpc port of consul tags after Conusl Registered
+   * */
+  @EventListener
+  public void listenInstanceRegisteredEvent(InstanceRegisteredEvent instanceRegisteredEvent){
+    if(alphaServerPort == 0){
+      if(instanceRegisteredEvent.getConfig() instanceof ConsulDiscoveryProperties){
+        ConsulDiscoveryProperties properties = (ConsulDiscoveryProperties)instanceRegisteredEvent.getConfig();
+        this.consuleInstanceId = formatConsulInstanceId(properties.getInstanceId());
+        Response<List<CatalogService>> services = consulClient.getCatalogService("servicecomb-alpha-server",null);
+        if(services.getValue().size()>0){
+          services.getValue().stream().filter(service ->
+              service.getServiceId().equalsIgnoreCase(this.consuleInstanceId)).forEach(service -> {
+
+            NewService newservice =  new NewService();
+            newservice.setName(service.getServiceName());
+            newservice.setId(service.getServiceId());
+            List<String> tags = consulDiscoveryProperties.getTags();
+            tags.remove("alpha-server-port=0");
+            tags.add("alpha-server-port="+actualAlphaServerPort);
+            newservice.setTags(tags);
+            consulClient.agentServiceRegister(newservice);
+          });
+        }
+      }
+    }
   }
 }
