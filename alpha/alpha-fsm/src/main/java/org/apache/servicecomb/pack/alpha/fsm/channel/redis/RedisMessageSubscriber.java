5732b233687c1b39d2ae4c4a8d5b19c0f35f2dcc
CMonkey <42.codemonkey@gmail.com>
2019-07-23 15:14:29 +0800

    add RedisMessageSubscriber.onMessage method

--- alpha/alpha-fsm/src/main/java/org/apache/servicecomb/pack/alpha/fsm/channel/redis/RedisMessageSubscriber.java
+++ alpha/alpha-fsm/src/main/java/org/apache/servicecomb/pack/alpha/fsm/channel/redis/RedisMessageSubscriber.java
@@ -1,4 +1,25 @@
 package org.apache.servicecomb.pack.alpha.fsm.channel.redis;
 
-public class RedisMessageSubscriber {
+import org.apache.servicecomb.pack.alpha.fsm.sink.ActorEventSink;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.connection.MessageListener;
+
+import java.nio.charset.StandardCharsets;
+
+public class RedisMessageSubscriber implements MessageListener {
+
+    private static final Logger logger = LoggerFactory.getLogger(RedisMessageSubscriber.class);
+
+    private ActorEventSink actorEventSink;
+
+    public RedisMessageSubscriber(ActorEventSink actorEventSink) {
+        this.actorEventSink = actorEventSink;
+    }
+
+    @Override
+    public void onMessage(Message message, byte[] pattern) {
+        logger.info("message = [{}] and pattern = [{}]", message.toString(), new String(pattern, StandardCharsets.UTF_8));
+    }
 }
