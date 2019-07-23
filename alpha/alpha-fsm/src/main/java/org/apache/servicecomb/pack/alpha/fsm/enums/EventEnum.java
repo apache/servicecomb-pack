22eba6b5e3d377aa5130da22500be866b5f6a670
CMonkey <42.codemonkey@gmail.com>
2019-07-23 17:31:12 +0800

    define event enum to EventEnum

--- alpha/alpha-fsm/src/main/java/org/apache/servicecomb/pack/alpha/fsm/enums/EventEnum.java
+++ alpha/alpha-fsm/src/main/java/org/apache/servicecomb/pack/alpha/fsm/enums/EventEnum.java
@@ -1,4 +1,18 @@
 package org.apache.servicecomb.pack.alpha.fsm.enums;
 
-public class EventEnum {
+public enum  EventEnum {
+    SagaStartedEvent("SagaStartedEvent"),
+    SagaEndedEvent("SagaEndedEvent"),
+    SagaAbortedEvent("SagaAbortedEvent"),
+    SagaTimeoutEvent("SagaTimeoutEvent"),
+    TxStartedEvent("TxStartedEvent"),
+    TxEndedEvent("TxEndedEvent"),
+    TxAbortedEvent("TxAbortedEvent"),
+    TxComponsitedEvent("TxComponsitedEvent");
+
+    private String event;
+
+    EventEnum(String event) {
+        this.event = event;
+    }
 }
