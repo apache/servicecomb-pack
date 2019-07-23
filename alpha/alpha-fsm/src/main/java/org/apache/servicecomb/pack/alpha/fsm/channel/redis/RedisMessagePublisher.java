686161d02cc9c34ba76ec8ae13fa58ecb73800cb
CMonkey <42.codemonkey@gmail.com>
2019-07-23 15:14:14 +0800

    add RedisMessagePublisher.publish method

--- alpha/alpha-fsm/src/main/java/org/apache/servicecomb/pack/alpha/fsm/channel/redis/RedisMessagePublisher.java
+++ alpha/alpha-fsm/src/main/java/org/apache/servicecomb/pack/alpha/fsm/channel/redis/RedisMessagePublisher.java
@@ -1,4 +1,27 @@
 package org.apache.servicecomb.pack.alpha.fsm.channel.redis;
 
-public class RedisMessagePublisher {
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+
+public class RedisMessagePublisher implements MessagePublisher {
+
+    private static final Logger logger = LoggerFactory.getLogger(RedisMessagePublisher.class);
+
+    private StringRedisTemplate stringRedisTemplate;
+    private ChannelTopic channelTopic;
+
+    public RedisMessagePublisher(StringRedisTemplate stringRedisTemplate, ChannelTopic channelTopic) {
+        this.stringRedisTemplate = stringRedisTemplate;
+        this.channelTopic = channelTopic;
+    }
+
+    @Override
+    public void publish(String message) {
+        logger.info("send message [{}] to [{}]", message, channelTopic.getTopic());
+        stringRedisTemplate.convertAndSend(channelTopic.getTopic(), message);
+
+    }
 }
