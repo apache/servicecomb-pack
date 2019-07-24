651d85901b846aeb3172bab66ac9c316664ff7af
CMonkey <42.codemonkey@gmail.com>
2019-07-24 10:28:27 +0800

    change RedisMessagePublisher.publish param type

--- alpha/alpha-fsm/src/main/java/org/apache/servicecomb/pack/alpha/fsm/channel/redis/RedisMessagePublisher.java
+++ alpha/alpha-fsm/src/main/java/org/apache/servicecomb/pack/alpha/fsm/channel/redis/RedisMessagePublisher.java
@@ -3,25 +3,25 @@ package org.apache.servicecomb.pack.alpha.fsm.channel.redis;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 
 public class RedisMessagePublisher implements MessagePublisher {
 
     private static final Logger logger = LoggerFactory.getLogger(RedisMessagePublisher.class);
 
-    private StringRedisTemplate stringRedisTemplate;
+    private RedisTemplate<String, Object> redisTemplate;
     private ChannelTopic channelTopic;
 
-    public RedisMessagePublisher(StringRedisTemplate stringRedisTemplate, ChannelTopic channelTopic) {
-        this.stringRedisTemplate = stringRedisTemplate;
+    public RedisMessagePublisher(RedisTemplate<String, Object> redisTemplate, ChannelTopic channelTopic) {
+        this.redisTemplate = redisTemplate;
         this.channelTopic = channelTopic;
     }
 
     @Override
-    public void publish(String message) {
-        logger.info("send message [{}] to [{}]", message, channelTopic.getTopic());
-        stringRedisTemplate.convertAndSend(channelTopic.getTopic(), message);
+    public void publish(Object data) {
+        logger.info("send message [{}] to [{}]", data, channelTopic.getTopic());
+        redisTemplate.convertAndSend(channelTopic.getTopic(), data);
 
     }
 }
