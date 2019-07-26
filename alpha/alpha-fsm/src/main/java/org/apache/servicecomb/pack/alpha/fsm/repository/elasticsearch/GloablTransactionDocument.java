9216dfee454a6d6902559d6e9057c414345d6221
Lei Zhang <coolbeevip@gmail.com>
2019-07-26 18:35:07 +0800

    SCB-1369 Define Elasticsearch Document Object for Query

---
+++ alpha/alpha-fsm/src/main/java/org/apache/servicecomb/pack/alpha/fsm/repository/elasticsearch/GloablTransactionDocument.java
@@ -0,0 +1,9 @@
+package org.apache.servicecomb.pack.alpha.fsm.repository.elasticsearch;
+
+import org.apache.servicecomb.pack.alpha.fsm.repository.model.GloablTransaction;
+import org.springframework.data.elasticsearch.annotations.Document;
+
+@Document(indexName = ElasticsearchTransactionRepository.INDEX_NAME, type = ElasticsearchTransactionRepository.INDEX_TYPE)
+public class GloablTransactionDocument extends GloablTransaction {
+
+}
