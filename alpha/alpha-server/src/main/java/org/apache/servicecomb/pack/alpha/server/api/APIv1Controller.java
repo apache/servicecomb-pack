c744e02299b040a1dd5ddb435f496ce209be352f
Lei Zhang <coolbeevip@gmail.com>
2019-08-08 23:50:53 +0800

    SCB-1411 Refactoring rest API

---
+++ alpha/alpha-server/src/main/java/org/apache/servicecomb/pack/alpha/server/api/APIv1Controller.java
@@ -0,0 +1,67 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.servicecomb.pack.alpha.server.api;
+
+import java.util.List;
+import java.util.Map;
+import org.apache.servicecomb.pack.alpha.core.metrics.AlphaMetrics;
+import org.apache.servicecomb.pack.alpha.core.fsm.repository.model.GlobalTransaction;
+import org.apache.servicecomb.pack.alpha.core.fsm.repository.model.PagingGlobalTransactions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/alpha/api/v1")
+public class APIv1Controller {
+
+  @Autowired
+  APIv1Impl APIv1Impl;
+
+  @GetMapping(value = "/getMetrics")
+  ResponseEntity<AlphaMetrics> metrics() {
+    return ResponseEntity.ok(APIv1Impl.getMetrics());
+  }
+
+  @GetMapping(value = "/transaction/{globalTxId}")
+  ResponseEntity<GlobalTransaction> getTransactionByGlobalTxId(@PathVariable String globalTxId)
+      throws Exception {
+    return ResponseEntity.ok(APIv1Impl.getTransactionByGlobalTxId(globalTxId));
+  }
+
+  @GetMapping(value = "/transaction")
+  ResponseEntity<PagingGlobalTransactions> getTransactions(@RequestParam(value = "page", required = false, defaultValue = "0") int page,
+      @RequestParam(value = "size", required = false, defaultValue = "50") int size)
+      throws Exception {
+    return ResponseEntity.ok(APIv1Impl.getTransactions(page,size));
+  }
+
+  @GetMapping(value = "/transaction/statistics")
+  ResponseEntity<Map<String,Long>> getTransactionStatistics() {
+    return ResponseEntity.ok(APIv1Impl.getTransactionStatistics());
+  }
+
+  @GetMapping(value = "/transaction/slow")
+  ResponseEntity<List<GlobalTransaction>> getSlowTransactions() {
+    return ResponseEntity.ok(APIv1Impl.getSlowTransactions());
+  }
+}
--- alpha/alpha-server/src/main/java/org/apache/servicecomb/pack/alpha/server/api/APIv1Impl.java
+++ alpha/alpha-server/src/main/java/org/apache/servicecomb/pack/alpha/server/api/APIv1Impl.java
@@ -19,22 +19,17 @@ package org.apache.servicecomb.pack.alpha.server.api;
 
 import java.util.List;
 import java.util.Map;
-import org.apache.servicecomb.pack.alpha.core.metrics.AlphaMetrics;
-import org.apache.servicecomb.pack.alpha.fsm.repository.TransactionRepository;
+import org.apache.servicecomb.pack.alpha.core.api.APIv1;
 import org.apache.servicecomb.pack.alpha.core.fsm.repository.model.GlobalTransaction;
 import org.apache.servicecomb.pack.alpha.core.fsm.repository.model.PagingGlobalTransactions;
+import org.apache.servicecomb.pack.alpha.core.metrics.AlphaMetrics;
+import org.apache.servicecomb.pack.alpha.fsm.repository.TransactionRepository;
 import org.apache.servicecomb.pack.alpha.server.metrics.AlphaMetricsEndpoint;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Component;
 
-@RestController
-@RequestMapping("/alpha/api/v1")
-public class APIControllerV1 {
+@Component
+public class APIv1Impl implements APIv1 {
 
   @Autowired
   AlphaMetricsEndpoint alphaMetricsEndpoint;
@@ -42,38 +37,32 @@ public class APIControllerV1 {
   @Autowired(required = false)
   TransactionRepository transactionRepository;
 
-  @GetMapping(value = "/metrics")
-  ResponseEntity<AlphaMetrics> metrics() {
+  public AlphaMetrics getMetrics() {
     AlphaMetrics alphaMetrics = new AlphaMetrics();
     alphaMetrics.setMetrics(alphaMetricsEndpoint.getMetrics());
     alphaMetrics.setNodeType(alphaMetricsEndpoint.getNodeType());
-    return ResponseEntity.ok(alphaMetrics);
+    return alphaMetrics;
   }
 
-  @GetMapping(value = "/transaction/{globalTxId}")
-  ResponseEntity<GlobalTransaction> getTransactionByGlobalTxId(@PathVariable String globalTxId)
+  public GlobalTransaction getTransactionByGlobalTxId(String globalTxId)
       throws Exception {
     GlobalTransaction globalTransaction = transactionRepository
         .getGlobalTransactionByGlobalTxId(globalTxId);
-    return ResponseEntity.ok(globalTransaction);
+    return globalTransaction;
   }
 
-  @GetMapping(value = "/transaction")
-  ResponseEntity<PagingGlobalTransactions> getTransactions(@RequestParam(value = "page", required = false, defaultValue = "0") int page,
-      @RequestParam(value = "size", required = false, defaultValue = "50") int size)
+  public PagingGlobalTransactions getTransactions(int page, int size)
       throws Exception {
     PagingGlobalTransactions pagingGlobalTransactions = transactionRepository
         .getGlobalTransactions(page, size);
-    return ResponseEntity.ok(pagingGlobalTransactions);
+    return pagingGlobalTransactions;
   }
 
-  @GetMapping(value = "/transaction/statistics")
-  ResponseEntity<Map<String,Long>> getTransactions() {
-    return ResponseEntity.ok(transactionRepository.getTransactionStatistics());
+  public Map<String, Long> getTransactionStatistics() {
+    return transactionRepository.getTransactionStatistics();
   }
 
-  @GetMapping(value = "/transaction/slow")
-  ResponseEntity<List<GlobalTransaction>> getSlowTransactions() {
-    return ResponseEntity.ok(transactionRepository.getSlowGlobalTransactionsTopN(10));
+  public List<GlobalTransaction> getSlowTransactions() {
+    return transactionRepository.getSlowGlobalTransactionsTopN(10);
   }
 }
