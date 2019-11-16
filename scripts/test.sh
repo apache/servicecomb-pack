e7addda2aa2a6d35efa34919289fdd527ce8397b
Daniel Qian <chanjarster@gmail.com>
2019-11-16 20:25:28 +0800

    SCB-1595 ServiceComb Pack integrates with SonarCloud

--- scripts/test.sh
+++ scripts/test.sh
@@ -22,7 +22,16 @@ then
   echo "Don't do anything here for the cron job!"
 else
   echo "Running the unit tests and integration tests here!"
-  mvn clean install -B -Pjacoco -Pdocker coveralls:report \
-    && mvn clean verify -B -f demo -Pdemo -Pdocker -Ddocker.useColor=false -Ddocker.showLogs \
-    && mvn clean verify -B -f acceptance-tests -Pdemo -Pdocker -Ddocker.useColor=false -Ddocker.showLogs
+  echo "TRAVIS_PULL_REQUEST=$TRAVIS_PULL_REQUEST"
+  if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+    echo "Not a pull request build, running build with sonar"
+    mvn clean install -B -Pjacoco -Pdocker coveralls:report sonar:sonar -Dsonar.projectKey=servicecomb-pack \
+      && mvn clean verify -B -f demo -Pdemo -Pdocker -Ddocker.useColor=false -Ddocker.showLogs \
+      && mvn clean verify -B -f acceptance-tests -Pdemo -Pdocker -Ddocker.useColor=false -Ddocker.showLogs
+  else
+    echo "Pull request build or local build"
+    mvn clean install -B -Pjacoco -Pdocker coveralls:report \
+      && mvn clean verify -B -f demo -Pdemo -Pdocker -Ddocker.useColor=false -Ddocker.showLogs \
+      && mvn clean verify -B -f acceptance-tests -Pdemo -Pdocker -Ddocker.useColor=false -Ddocker.showLogs
+  fi;
 fi
